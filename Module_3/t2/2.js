742044ed2b4b30cf0f25db6125f7b740e11855c5
markferdo <mark93lk@gmail.com>
2023-12-10 15:54:16 +0200

    1st

---
+++ Module_3/t2/2.js
@@ -0,0 +1,11 @@
+// module 3
+const target = document.getElementById('target');
+
+const body = `
+  <li>First item</li>
+  <li>Second item</li>
+  <li>Third item</li>
+`;
+
+target.innerHTML = body;
+target.classList.add('my-list');
---
+++ Module_3/t3/3.js
@@ -0,0 +1,9 @@
+'use strict';
+const names = ['John', 'Paul', 'Jones'];
+const target = document.getElementById('target');
+
+for (let i = 0; i < names.length; i++) {
+    let listItem = document.createElement('li');
+    listItem.textContent = names[i];
+    target.appendChild(listItem);
+}
